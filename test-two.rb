080a9512deccfd3042d3691121527b50825ea2f2
chris-wamae <chriswamae123@gmail.com>
2023-02-08 15:19:25 +0300

    Completed second Hackerrank test

---
+++ test-two.rb
@@ -0,0 +1,24 @@
+#BDD - From colors, return a list that has  all of the values
+
+#PSEUDOCODE
+
+#START
+#1.Create an empty array the iterate_colors method
+#2. Iterate through colors using each
+#3. Pass it two arguments
+#4. Get the values from the second argument
+#5. Add the values to an array 
+#6. Return the array of values
+#END
+
+#TEST CASES
+#Use puts on the return to print it out and check if its an array of #values
+
+def iterate_colors(colors)
+    # Your code here
+     color_values = []
+     colors.each do |v|
+          color_values << v
+     end
+      color_values
+  end
